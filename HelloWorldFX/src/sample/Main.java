a78e1fca6e9c245f1116d3b9f5fc6a35c896ba44
SkyluxSky <skyluxsky@gmail.com>
2022-01-02 13:49:05 -0500

    Used MVC to create and modify a Label. Set up JavaFX Application in Code

--- HelloWorldFX/src/sample/Main.java
+++ HelloWorldFX/src/sample/Main.java
@@ -2,8 +2,14 @@ package sample;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Pos;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -12,9 +18,20 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+
+        //Creates fxml configurations in code variation
+//        GridPane root = new GridPane();
+//        root.setAlignment(Pos.CENTER);
+//        root.setVgap(10);
+//        root.setHgap(10);
+//
+//        Label greeting = new Label("Welcome to JavaFX!");
+//        greeting.setTextFill(Color.GREEN);// sets labels color as green
+//        greeting.setFont(Font.font("Times New Roman", FontWeight.BOLD, 70)); //Sets font and font weight
+//        root.getChildren().add(greeting);//Adds label to root grid pane
         //Window Title
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));//New Scene and Stage
+        primaryStage.setTitle("Hello JavaFX!");
+        primaryStage.setScene(new Scene(root, 700, 275));//New Scene and Stage(sets width and height od window)
         primaryStage.show();
     }
 
