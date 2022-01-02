d8e5bbda7760b673aa8d0503f976dacffbb0cfaa
SkyluxSky <skyluxsky@gmail.com>
2022-01-02 12:51:51 -0500

    Configured JavaFX in Java 11

---
+++ HelloWorldFX/src/module-info.java
@@ -0,0 +1,8 @@
+//Module Set Up for JavaFX
+//Copy this file each time you want to use JavaFX
+module HelloWorldFX {
+    requires javafx.fxml;
+    requires javafx.controls;
+
+    opens sample;
+}
